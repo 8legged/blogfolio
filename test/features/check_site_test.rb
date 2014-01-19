bcc315383dbba7bc051a9898ad5c30eb43c06078
8legged <jason.niemi@live.com>
2014-01-19 08:05:58 -0800

    fixed failing tests

--- test/features/check_site_test.rb
+++ test/features/check_site_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
  # class CheckSiteTest < Capybara::Rails::TestCase
   feature "A public facing site" do
     scenario "has a welcome headline" do
-      visit "http://google.com"
+      visit posts_path
       page.text.must_include "Listing posts"
     end
   end
