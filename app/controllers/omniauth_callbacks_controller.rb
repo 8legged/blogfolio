8cadb3b006a18bdc6173109c62185034cb4002a7
8legged <jason.niemi@live.com>
2014-12-10 13:21:56 -0800

    removed personalized welcome from omniauth callback since it broke my test

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # raise request.env["omniauth.auth"].to_yaml
 
     if user.persisted?
-      flash[:notice] = "#{user.name}, you're signed in!"
+      flash[:notice] = "Signed in!"
       sign_in_and_redirect user, notice: "#{user.name}, you are signed in!"
     else
       session["devise.user_attributes"] = user.attributes
